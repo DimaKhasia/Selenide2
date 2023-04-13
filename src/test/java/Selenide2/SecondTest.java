19f38f953edfb11ea74cd14b800b05d9bbc5e1c6
Dmitrii Khasia <dihasiya@mail.ru>
2023-04-14 00:07:09 +0400

    Main commit

--- src/test/java/Selenide2/SecondTest.java
+++ src/test/java/Selenide2/SecondTest.java
@@ -2,12 +2,20 @@ package Selenide2;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SecondTest {
     @Test
-    void draganddrop() {
+    void dragAndDrop() {
+        // Открыть страницу https://the-internet.herokuapp.com/drag_and_drop
         open ("https://the-internet.herokuapp.com/drag_and_drop");
+        // Переместить прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+        // Проверка, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
 
 }
 }
